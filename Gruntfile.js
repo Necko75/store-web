daf288310eb8802f96f858252aaf846ac21dec80
costa simon <costapasquier.simon@gmail.com>
2015-06-07 13:27:07 +0200

    feat(main): add grunt concat, gruntfile tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,9 +10,35 @@ module.exports = function (grunt) {
 			build: {
 				src: [ dist, tmp ]
 			}
+		},
+
+		concat: {
+			build: {
+				src: [
+					app + '/root/app.js',
+					app + '/**/*-module.js',
+					app + '/**/*-service.js',
+					app + '/**/*-utils.js',
+					app + '/**/*-controller.js',
+					app + '/**/*-directive.js'
+
+				],
+				dest: dist + '/js/app.js'
+			},
+			vendor: {
+				src: [
+					components + '/angular/angular.js',
+					components + '/angular-ui-router/release/angular-ui-router.js'
+				],
+				dest: dist + '/js/vendor.js'
+			}
 		}
 	});
 
+	// loaded tasks //
 	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.registerTask('default', [ 'clean' ]);
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	// registered tasks //
+	grunt.registerTask('default', [ 'clean', 'concat' ]);
 };
