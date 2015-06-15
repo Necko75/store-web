739e6671d6baab089b8b775667b305c764da5c9f
costa simon <costapasquier.simon@gmail.com>
2015-06-15 22:36:39 +0200

    feat(login): modify url for account session module

--- app/components/services/sessions-service.js
+++ app/components/services/sessions-service.js
@@ -1,6 +1,6 @@
 app.factory('Sessions', function ($http, apiUrls) {
 	return {
-		apiLogin: apiUrls.base + '/users',
+		apiLogin: apiUrls.base + '/account',
 
 		loginAction: function (userData) {
 			$http.post(this.apiLogin + '/login', { user: userData }).then(function (res) {
