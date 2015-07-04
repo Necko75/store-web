f114adeb0c0e421dd56efecada10e67b4f9755ca
costa simon <costapasquier.simon@gmail.com>
2015-07-04 13:07:37 +0200

    feat(galleries): display all galleries user from state galleries.list

--- app/myspace/galleries/list/gallery-list-controller.js
+++ app/myspace/galleries/list/gallery-list-controller.js
@@ -1,5 +1,4 @@
 myspaceGalleryList.controller('GalleryListController', function ($scope, galleriesData) {
 	$scope.galleries = galleriesData;
-	$scope.coucou = "fdffd";
 	console.log($scope.galleries);
 });
